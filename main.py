f58d5f108a1edaaee4263bcf5b7b20d5b6cfd66e
WerewolfHunters <awwab.mahimi.0074@gmail.com>
2025-08-17 18:51:58 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
-# from networksecurity.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation
 from src.exception.exception import NetworkSecurityException
 from src.logging.logger import logging
-from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from src.entity.config_entity import TrainingPipelineConfig
 
 # from networksecurity.components.model_trainer import ModelTrainer
@@ -30,6 +30,14 @@ if __name__=='__main__':
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+
+        # Data Transformation phase
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Initiate the data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(data_transformation_artifact)
 
 
     except Exception as e:
